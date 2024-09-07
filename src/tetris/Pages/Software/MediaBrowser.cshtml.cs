b497fb695bc936c3ca5cb03a575d3cd512db634f
v38armageddon <florian.m@v38armageddon.net>
2024-09-07 16:09:02 +0200

    this free up spaces on github

---
+++ src/tetris/Pages/Software/MediaBrowser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tetris.Pages.Software
+{
+    public class MediaBrowserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
