f0dd372fded8e68c848602138455e4208b15a629
v38armageddon <florian.m@v38armageddon.net>
2025-07-17 21:21:27 +0200

    [POLICY] Update TOS + Privacy Policy and add Content Policy

---
+++ src/tetris/Pages/Content-Policy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tetris.Pages
+{
+    public class Content_PolicyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
